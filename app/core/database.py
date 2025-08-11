3ddcedd9c1bdd834e63b5fe35c8af7317727c5e5
GuyVanced <pandeykushal017@gmail.com>
2025-08-11 16:35:53 +0545

    feat : Database Connection using SQLAlchemy

--- app/core/database.py
+++ app/core/database.py
@@ -1,1 +1,25 @@
 # database connecitons setup
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+sqlite_db_name = "task.db"
+sqlite_url = f"sqlite:///{sqlite_db_name}"
+
+connect_args = {"check_same_thread": False}
+
+engine = create_engine(sqlite_url,connect_args=connect_args)
+
+session = sessionmaker(engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+    

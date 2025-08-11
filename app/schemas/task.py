a092dcc832c223b610863f9ada21f90f61b06181
GuyVanced <pandeykushal017@gmail.com>
2025-08-11 18:52:45 +0545

    feat : created Pydantic schemas for Task

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,1 +1,28 @@
 # pydantic schemas
+from pydantic import BaseModel
+from typing import Optional
+
+class TaskBase(BaseModel):
+    title : str
+    description : str
+    completed : bool
+
+class CreateTask(TaskBase):
+    pass
+
+class UpdateTask(BaseModel):
+    title: Optional[str] = None
+    description : Optional[str] = None
+    completed : Optional[bool] = None
+
+class TaskResponse(TaskBase):
+    id : int
+
+    class Config():
+        orm_mode = True
+
+
+
+
+
+

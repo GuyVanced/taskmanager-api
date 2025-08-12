82bf3a19d326078fd1756cc35d735a04c2c91000
GuyVanced <pandeykushal017@gmail.com>
2025-08-12 14:22:33 +0545

    feat: Added title validation for creating and updating tasks (min 3 characters)

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,22 +1,24 @@
 # pydantic schemas
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class TaskBase(BaseModel):
-    title : str
+    title : str = Field(...,min_length=3, description="Title must be atleast 3 characters")
     description : str
     completed : bool
 
 class CreateTask(TaskBase):
     pass
 
+# To allow users to optionally update choosen fields
 class UpdateTask(BaseModel):
-    title: Optional[str] = None
+    title : str = Field(None,min_length=3, description="Title must be atleast 3 characters")
     description : Optional[str] = None
     completed : Optional[bool] = None
 
+# To return all Task details including the id
 class TaskResponse(TaskBase):
-    id : int
+    task_id : int
 
     class Config():
         orm_mode = True
